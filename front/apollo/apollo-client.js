1d890d8add043f38fdd21a56f151887b8b11633b
aliaksei.badnarchuk <aliaksei.badnarchuk@calydo.com>
2022-11-16 08:54:42 +0100

    change https

--- front/apollo/apollo-client.js
+++ front/apollo/apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
 const client = new ApolloClient({
-    uri: 'http://staging.immokonfigurator.ch/api',
+    uri: 'https://staging.immokonfigurator.ch/api',
     cache: new InMemoryCache({resultCaching: true}),
 });
 
